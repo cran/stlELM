c0ec0e19a5761fa89766918d9b237510781416c8
Girish Kumar Jha <girish.stat@gmail.com>
2021-02-22 10:10:02 +0000

    version 0.1.0

---
+++ R/STLELM.R
@@ -0,0 +1,31 @@
+#' @importFrom forecast mstl
+#' @importFrom nnfor elm
+#' @importFrom utils head tail
+#' @importFrom graphics plot
+#' @importFrom stats as.ts ts
+#' @export
+#'
+STLELM <- function(data, stepahead=10){
+  STLcomp <- mstl(data)
+  STLcomp_plots<-plot(STLcomp)
+  data_trn <- ts(head(data, round(length(data) - stepahead)))
+  data_test <- ts(tail(data, stepahead))
+  STLcomp_trn <- STLcomp[-c(((length(data)-stepahead)+1):length(data)),]
+  Fcast_STLcomp <- NULL
+  for (STLcomp in 2:ncol(STLcomp_trn)) {
+    Indcomp <- NULL
+    Indcomp <- STLcomp_trn[ ,STLcomp]
+    stlELMFit <- nnfor::elm(as.ts(Indcomp))
+    stlELM_fcast=forecast::forecast(stlELMFit, h=stepahead)
+    stlELM_fcast_Mean=stlELM_fcast$mean
+    Fcast_STLcomp <- cbind(Fcast_STLcomp, as.matrix(stlELM_fcast_Mean))
+  }
+  FinalstlELM_fcast <- ts(rowSums(Fcast_STLcomp, na.rm = T))
+  MAE_stlELM=mean(abs(data_test - FinalstlELM_fcast))
+  MAPE_stlELM=mean(abs(data_test - FinalstlELM_fcast)/data_test)
+  rmse_stlELM=sqrt(mean((data_test - FinalstlELM_fcast)^2))
+  return(list(data_test=data_test, STLcomp_forecast=Fcast_STLcomp,
+              FinalSTLcomp_forecast=FinalstlELM_fcast, MAE_stlELM=MAE_stlELM,
+              MAPE_stlELM=MAPE_stlELM, rmse_stlELM=rmse_stlELM,
+              STLcomp_plots=STLcomp_plots))
+}
